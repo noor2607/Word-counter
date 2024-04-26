50807b44bae94ca5b51b1693c120e7a8f8a7e86d
Lareb Noor <156116687+noor2607@users.noreply.github.com>
2024-04-26 22:36:20 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,10 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+const answers = await inquirer.prompt([{
+        name: "sentence",
+        type: "input",
+        message: "Enter Your Sentence to count total words"
+    }]);
+const words = answers.sentence.trim().split(" ");
+console.log(words);
+console.log(`Your Sentence total words are ${words.length}`);
